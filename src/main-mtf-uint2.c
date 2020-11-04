06f8fbdf6fe39c884017ae3773041b184558d2dc
jmcmillan1 <47255714+jmcmillan1@users.noreply.github.com>
2020-11-04 15:04:14 -0700

    Add main tests for uint2-8

---
+++ src/main-mtf-uint2.c
@@ -0,0 +1,10 @@
+
+#define T uint2
+#define LEN (10)
+
+#include "mtf.c"
+
+int main()
+{
+    test_mtf();
+}
---
+++ src/main-mtf-uint4.c
@@ -0,0 +1,10 @@
+
+#define T uint4
+#define LEN (10)
+
+#include "mtf.c"
+
+int main()
+{
+    test_mtf();
+}
