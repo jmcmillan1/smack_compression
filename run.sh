15cdf2c68a3b259a9cfff47cb895824ce8bd36e2
jmcmillan1 <47255714+jmcmillan1@users.noreply.github.com>
2020-11-12 15:39:09 -0700

    adding run script for old algorithms

---
+++ run.sh
@@ -0,0 +1,180 @@
+make smoke-corral-logs/bit-uint1-log.txt
+make smoke-corral-logs/bit-uint2-log.txt
+make smoke-corral-logs/bit-uint4-log.txt
+make smoke-corral-logs/bit-uint8-log.txt
+make smoke-boogie-logs/bit-uint1-log.txt
+make smoke-boogie-logs/bit-uint2-log.txt
+make smoke-boogie-logs/bit-uint4-log.txt
+make smoke-boogie-logs/bit-uint8-log.txt
+
+make smoke-corral-logs/inv-uint1-log.txt
+make smoke-corral-logs/inv-uint2-log.txt
+make smoke-corral-logs/inv-uint4-log.txt
+make smoke-corral-logs/inv-uint8-log.txt
+make smoke-boogie-logs/inv-uint1-log.txt
+make smoke-boogie-logs/inv-uint2-log.txt
+make smoke-boogie-logs/inv-uint4-log.txt
+make smoke-boogie-logs/inv-uint8-log.txt
+
+make smoke-corral-logs/lvs-rle-uint1-log.txt
+make smoke-corral-logs/lvs-rle-uint2-log.txt
+make smoke-corral-logs/lvs-rle-uint4-log.txt
+make smoke-corral-logs/lvs-rle-uint8-log.txt
+make smoke-boogie-logs/lvs-rle-uint1-log.txt
+make smoke-boogie-logs/lvs-rle-uint2-log.txt
+make smoke-boogie-logs/lvs-rle-uint4-log.txt
+make smoke-boogie-logs/lvs-rle-uint8-log.txt
+
+make smoke-corral-logs/lvs-uint1-log.txt
+make smoke-corral-logs/lvs-uint2-log.txt
+make smoke-corral-logs/lvs-uint4-log.txt
+make smoke-corral-logs/lvs-uint8-log.txt
+make smoke-boogie-logs/lvs-uint1-log.txt
+make smoke-boogie-logs/lvs-uint2-log.txt
+make smoke-boogie-logs/lvs-uint4-log.txt
+make smoke-boogie-logs/lvs-uint8-log.txt
+
+make smoke-corral-logs/rle-uint1-log.txt
+make smoke-corral-logs/rle-uint2-log.txt
+make smoke-corral-logs/rle-uint4-log.txt
+make smoke-corral-logs/rle-uint8-log.txt
+make smoke-boogie-logs/rle-uint1-log.txt
+make smoke-boogie-logs/rle-uint2-log.txt
+make smoke-boogie-logs/rle-uint4-log.txt
+make smoke-boogie-logs/rle-uint8-log.txt
+
+make fail-corral-logs/bit-uint1-log.txt
+make fail-corral-logs/bit-uint2-log.txt
+make fail-corral-logs/bit-uint4-log.txt
+make fail-corral-logs/bit-uint8-log.txt
+make fail-boogie-logs/bit-uint1-log.txt
+make fail-boogie-logs/bit-uint2-log.txt
+make fail-boogie-logs/bit-uint4-log.txt
+make fail-boogie-logs/bit-uint8-log.txt
+
+make fail-corral-logs/inv-uint1-log.txt
+make fail-corral-logs/inv-uint2-log.txt
+make fail-corral-logs/inv-uint4-log.txt
+make fail-corral-logs/inv-uint8-log.txt
+make fail-boogie-logs/inv-uint1-log.txt
+make fail-boogie-logs/inv-uint2-log.txt
+make fail-boogie-logs/inv-uint4-log.txt
+make fail-boogie-logs/inv-uint8-log.txt
+
+make fail-corral-logs/lvs-rle-uint1-log.txt
+make fail-corral-logs/lvs-rle-uint2-log.txt
+make fail-corral-logs/lvs-rle-uint4-log.txt
+make fail-corral-logs/lvs-rle-uint8-log.txt
+make fail-boogie-logs/lvs-rle-uint1-log.txt
+make fail-boogie-logs/lvs-rle-uint2-log.txt
+make fail-boogie-logs/lvs-rle-uint4-log.txt
+make fail-boogie-logs/lvs-rle-uint8-log.txt
+
+make fail-corral-logs/lvs-uint1-log.txt
+make fail-corral-logs/lvs-uint2-log.txt
+make fail-corral-logs/lvs-uint4-log.txt
+make fail-corral-logs/lvs-uint8-log.txt
+make fail-boogie-logs/lvs-uint1-log.txt
+make fail-boogie-logs/lvs-uint2-log.txt
+make fail-boogie-logs/lvs-uint4-log.txt
+make fail-boogie-logs/lvs-uint8-log.txt
+
+make fail-corral-logs/rle-uint1-log.txt
+make fail-corral-logs/rle-uint2-log.txt
+make fail-corral-logs/rle-uint4-log.txt
+make fail-corral-logs/rle-uint8-log.txt
+make fail-boogie-logs/rle-uint1-log.txt
+make fail-boogie-logs/rle-uint2-log.txt
+make fail-boogie-logs/rle-uint4-log.txt
+make fail-boogie-logs/rle-uint8-log.txt
+
+make hard-corral-logs/bit-uint1-log.txt
+make hard-corral-logs/bit-uint2-log.txt
+make hard-corral-logs/bit-uint4-log.txt
+make hard-corral-logs/bit-uint8-log.txt
+make hard-boogie-logs/bit-uint1-log.txt
+make hard-boogie-logs/bit-uint2-log.txt
+make hard-boogie-logs/bit-uint4-log.txt
+make hard-boogie-logs/bit-uint8-log.txt
+
+make hard-corral-logs/inv-uint1-log.txt
+make hard-corral-logs/inv-uint2-log.txt
+make hard-corral-logs/inv-uint4-log.txt
+make hard-corral-logs/inv-uint8-log.txt
+make hard-boogie-logs/inv-uint1-log.txt
+make hard-boogie-logs/inv-uint2-log.txt
+make hard-boogie-logs/inv-uint4-log.txt
+make hard-boogie-logs/inv-uint8-log.txt
+
+make hard-corral-logs/lvs-rle-uint1-log.txt
+make hard-corral-logs/lvs-rle-uint2-log.txt
+make hard-corral-logs/lvs-rle-uint4-log.txt
+make hard-corral-logs/lvs-rle-uint8-log.txt
+make hard-boogie-logs/lvs-rle-uint1-log.txt
+make hard-boogie-logs/lvs-rle-uint2-log.txt
+make hard-boogie-logs/lvs-rle-uint4-log.txt
+make hard-boogie-logs/lvs-rle-uint8-log.txt
+
+make hard-corral-logs/lvs-uint1-log.txt
+make hard-corral-logs/lvs-uint2-log.txt
+make hard-corral-logs/lvs-uint4-log.txt
+make hard-corral-logs/lvs-uint8-log.txt
+make hard-boogie-logs/lvs-uint1-log.txt
+make hard-boogie-logs/lvs-uint2-log.txt
+make hard-boogie-logs/lvs-uint4-log.txt
+make hard-boogie-logs/lvs-uint8-log.txt
+
+make hard-corral-logs/rle-uint1-log.txt
+make hard-corral-logs/rle-uint2-log.txt
+make hard-corral-logs/rle-uint4-log.txt
+make hard-corral-logs/rle-uint8-log.txt
+make hard-boogie-logs/rle-uint1-log.txt
+make hard-boogie-logs/rle-uint2-log.txt
+make hard-boogie-logs/rle-uint4-log.txt
+make hard-boogie-logs/rle-uint8-log.txt
+
+make pass-corral-logs/bit-uint1-log.txt
+make pass-corral-logs/bit-uint2-log.txt
+make pass-corral-logs/bit-uint4-log.txt
+make pass-corral-logs/bit-uint8-log.txt
+make pass-boogie-logs/bit-uint1-log.txt
+make pass-boogie-logs/bit-uint2-log.txt
+make pass-boogie-logs/bit-uint4-log.txt
+make pass-boogie-logs/bit-uint8-log.txt
+
+make pass-corral-logs/inv-uint1-log.txt
+make pass-corral-logs/inv-uint2-log.txt
+make pass-corral-logs/inv-uint4-log.txt
+make pass-corral-logs/inv-uint8-log.txt
+make pass-boogie-logs/inv-uint1-log.txt
+make pass-boogie-logs/inv-uint2-log.txt
+make pass-boogie-logs/inv-uint4-log.txt
+make pass-boogie-logs/inv-uint8-log.txt
+
+make pass-corral-logs/lvs-rle-uint1-log.txt
+make pass-corral-logs/lvs-rle-uint2-log.txt
+make pass-corral-logs/lvs-rle-uint4-log.txt
+make pass-corral-logs/lvs-rle-uint8-log.txt
+make pass-boogie-logs/lvs-rle-uint1-log.txt
+make pass-boogie-logs/lvs-rle-uint2-log.txt
+make pass-boogie-logs/lvs-rle-uint4-log.txt
+make pass-boogie-logs/lvs-rle-uint8-log.txt
+
+make pass-corral-logs/lvs-uint1-log.txt
+make pass-corral-logs/lvs-uint2-log.txt
+make pass-corral-logs/lvs-uint4-log.txt
+make pass-corral-logs/lvs-uint8-log.txt
+make pass-boogie-logs/lvs-uint1-log.txt
+make pass-boogie-logs/lvs-uint2-log.txt
+make pass-boogie-logs/lvs-uint4-log.txt
+make pass-boogie-logs/lvs-uint8-log.txt
+
+make pass-corral-logs/rle-uint1-log.txt
+make pass-corral-logs/rle-uint2-log.txt
+make pass-corral-logs/rle-uint4-log.txt
+make pass-corral-logs/rle-uint8-log.txt
+make pass-boogie-logs/rle-uint1-log.txt
+make pass-boogie-logs/rle-uint2-log.txt
+make pass-boogie-logs/rle-uint4-log.txt
+make pass-boogie-logs/rle-uint8-log.txt
+
