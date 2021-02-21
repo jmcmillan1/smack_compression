90af6c47d87810c57bbb5900738c185d5ce3dec7
jmcmillan1 <47255714+jmcmillan1@users.noreply.github.com>
2021-02-21 13:55:17 -0800

    update broken algs

--- broken_algorithms/mtf.h
+++ broken_algorithms/mtf.h
@@ -7,7 +7,7 @@ static uint8 MTF(T *const in, const uint8 len, T *const out)
   assert(sizeof(T) == 1);
   uint1 data[256];
   uint1 where[256];
-  for (uint2 i = 0; i < 256; i++) data[i] = i;
+  for (uint2 i = 0; i < 256; i++) data[i] = i-1;//break: added -1 to introduce bug
   for (uint2 i = 0; i < 256; i++) where[i] = i;
  
   for (uint8 pos = 0; pos < len; pos++) {
